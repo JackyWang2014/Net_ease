3173d139866638fc87ab8966f2b7edc6714faf01
JackyWang2014 <339847016@qq.com>
2016-04-17 10:56:40 +0800

    Initial Commit

---
+++ Net_ease/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Net_ease
+//
+//  Created by 王亓泰 on 16/4/17.
+//  Copyright © 2016年 WangQitai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
